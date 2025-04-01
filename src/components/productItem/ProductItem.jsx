67eeaf9abc363dab5aa16b76da6b9bf5404d6a09
nerina <ncoronel@primerosystems.com>
2025-04-01 19:02:36 -0300

    Added state for availability

--- src/components/productItem/ProductItem.jsx
+++ src/components/productItem/ProductItem.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ProductItem = (props) => {
+const ProductItem = ({ name, price, details }) => {
+    const [available, setAvailable] = useState(true);
+
     return (
         <div className='product-item'>
-            <h2>{props.name}</h2>
-            <h3>${props.price}</h3>
-            <p>{props.details}</p>
-            <p>{props.dollar}</p>
+            <h2>{name}</h2>
+            <h3>${price}</h3>
+            <p>{details}</p>
+            <p>Stock: {available ? "Available" : "Unavailable"}</p>
+            <button onClick={() => setAvailable(!available)}>Change stock</button>
         </div>
     )
 }
