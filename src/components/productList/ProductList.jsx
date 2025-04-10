1a458ebd47612b8cb2aeaf8545c92ff6ed084c97
nerina <ncoronel@primerosystems.com>
2025-04-10 20:10:06 -0300

    Added bootstrap

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import ProductItem from '../productItem/ProductItem';
+import { Col, Container, Row } from 'react-bootstrap';
 
 
-const ProductList = ({products, dollar}) => {
+const ProductList = ({ products, dollar }) => {
   return (
-    <>
-     {products.map((product) => (
-        <ProductItem
-        dollar={dollar}
-        name={product.name}
-        price={product.price}
-        details={product.details}
-        />
-     ))}
-    </>
+    <Container>
+      <Row>
+        {products.map((product, index) => (
+          <Col key={index} xl={4} md={6} xs={12}>
+            <ProductItem
+              dollar={dollar}
+              name={product.name}
+              price={product.price}
+              details={product.details}
+            />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   )
 }
 
