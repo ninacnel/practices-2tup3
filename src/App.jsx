7c511d50c15117a77dbaad25e14234f5c37d323a
nerina <ncoronel@primerosystems.com>
2025-04-15 16:44:43 -0300

    Added dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-import { useState } from 'react';
 import './App.css'
-import BeerStyles from './components/beerStyles/BeerStyles';
-import Footer from './components/footer/Footer'
-import ProductList from './components/productList/ProductList'
-import ChangeDollar from './components/changeDollar/ChangeDollar';
-import { allProducts, beers } from './components/data';
-import ProductForm from './components/productForm/ProductForm';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
-  const [products, setProducts] = useState(allProducts);
-  const [hideDollar, setHideDollar] = useState(true);
-  const [dollar, setDollar] = useState(1200);
-
-  const handleAddProduct = (newProduct) =>{
-      setProducts([...products, newProduct]);
-  }
-
   return (
-    <>
-      <h1>Bazar</h1>
-      <button className="btn btn-danger" onClick={() => setHideDollar(!hideDollar)}>Hide Dollar</button>
-      <div className='product-list'>
-        <ProductList products={products} dollar={dollar}/>
-        {/* <ProductForm onAddNewProduct={handleAddProduct}/>
-        {hideDollar && (<ChangeDollar onSetNewDollar={setDollar}/>)}
-        <br />
-        <br />
-        <BeerStyles beers={beers} /> */}
-      </div>
-      <Footer />
-    </>
+    <Dashboard />
   )
 }
 
